cefb84e3058bb5e3a8fd1366c26f9a7a67fc0331
shane <michealax@github.com>
2024-06-16 20:10:34 +0800

    init

---
+++ dubbo-provider/src/test/java/com/shane/alibaba/DubboDempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shane.alibaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboDempApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
