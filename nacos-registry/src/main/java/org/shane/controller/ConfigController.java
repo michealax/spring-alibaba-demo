5190fba99cfbc95034a4d5f087cd7f5789675e11
shane <michealax@github.com>
2024-06-20 16:00:07 +0800

    nacos

--- nacos-registry/src/main/java/org/shane/controller/ConfigController.java
+++ nacos-registry/src/main/java/org/shane/controller/ConfigController.java
@@ -1,6 +1,5 @@
 package org.shane.controller;
 
-import com.alibaba.nacos.api.config.annotation.NacosValue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
